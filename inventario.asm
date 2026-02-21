b3fcc84377708cba26acf8d4713548af57170373
Ángel Pacheco <camargopache@gmail.com>
2026-02-21 02:32:00 -0400

    Add files via upload

---
+++ inventario.asm
@@ -0,0 +1,22 @@
+.data
+P001: .word 1234, 320, 0, 75
+      .asciiz "Jugo de manzana"
+      .align 5
+
+P002: .word 5656, 1023, 1, 23
+      .asciiz "Galletas"
+      .align 5
+
+P003: .word 9999, 106, 2, 57
+      .asciiz "Corn Flakes"
+      .align 5
+
+P004: .word 1111, 30, 5, 0
+      .asciiz "Leche 0.5L"
+      .align 5
+      
+P005: .word 1121, 58, 0, 50
+      .asciiz "Queso Blanco"
+      .align 5
+
+.word 0
